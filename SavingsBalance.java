4ef67d085bb8003ead763b36912c51ccec07c7c5
Itamar Junior <itjunior@gmail.com>
2021-02-01 22:09:05 +0000

    updating

--- SavingsBalance.java
+++ SavingsBalance.java
@@ -7,14 +7,14 @@ public class SavingsBalance
     double itamarWorkRate = 11.2;
     int itamarRent = 450;
     int itamarShoppingWeek = 100;
-    
+
     double itamarWorkMonthIncome = itamarWorkRate * itamarWorkHoursWeek * 4;
     int itamarSpendMont = itamarShoppingWeek * 4;
-    
+
     System.out.println("Itamar actual saving is "+ itamarSavings +"€");
     System.out.println("Itamars income per month is " + itamarWorkMonthIncome +"€, per week is " +itamarShoppingWeek+"€");
     System.out.println("Itamars spend per month is " + itamarSpendMont+"€");
-    
+
     double itamarSavings3Month = (itamarWorkMonthIncome*3) - (itamarSpendMont*3) +itamarSavings;
     System.out.println("Itamars saving after 3 months is " + itamarSavings3Month +"€");
 
