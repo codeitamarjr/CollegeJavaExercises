000fdb60922470dccaa40283149be9f96432991e
Itamar Junior <itjunior@gmail.com>
2021-02-01 18:18:43 +0000

    Last code lines.

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,7 +1,10 @@
-public class HelloWorld // The main method bellong the class HelloWorld, all of this can be inside one package
+public class HelloWorld // class is the basic block of a java program, it's contain method.
 {
-    public static void main(String[] args) // This is the main method.
+    public static void main(String[] args) // Every program must have a main method, it's where the program start.
     {
         System.out.println("Hello World");
+
+        System.out.println("Hello\nWorld\nhow are you today?"); // "\n" will jump for the next line, also prntln will jump one line at the end of this command.
+
     }
 }
---
+++ SavingsBalance.java
@@ -0,0 +1,22 @@
+public class SavingsBalance
+{
+    public static void main(String[] args) // Every program must have a main method, it's where the program start.
+{
+    int itamarSavings = 1035;
+    byte itamarWorkHoursWeek = 29;
+    double itamarWorkRate = 11.2;
+    int itamarRent = 450;
+    int itamarShoppingWeek = 100;
+    
+    double itamarWorkMonthIncome = itamarWorkRate * itamarWorkHoursWeek * 4;
+    int itamarSpendMont = itamarShoppingWeek * 4;
+    
+    System.out.println("Itamar actual saving is "+ itamarSavings +"€");
+    System.out.println("Itamars income per month is " + itamarWorkMonthIncome +"€, per week is " +itamarShoppingWeek+"€");
+    System.out.println("Itamars spend per month is " + itamarSpendMont+"€");
+    
+    double itamarSavings3Month = (itamarWorkMonthIncome*3) - (itamarSpendMont*3) +itamarSavings;
+    System.out.println("Itamars saving after 3 months is " + itamarSavings3Month +"€");
+
+        }
+}
